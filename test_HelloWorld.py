abb801f76df5cb230f23f13a8f09ac4666782b7d
Niklas Wehl <niklaswehl@outlook.de>
2024-11-03 19:53:50 +0100

    Neuer Code

--- test_HelloWorld.py
+++ test_HelloWorld.py
@@ -1,9 +1,22 @@
-a = 1
-b = 2
-c = a+b
+import unittest
 
-print("Hello World " + str(c))
-print(a)
+# Funktion, die getestet werden soll
+def add(a, b):
+    return a + b
 
-def test_dummy():
-    pass
+# Testklasse, die von unittest.TestCase erbt
+class TestAddFunction(unittest.TestCase):
+
+    # Testmethoden beginnen mit "test_"
+    def test_add_positive_numbers(self):
+        self.assertEqual(add(2, 3), 5)
+
+    def test_add_negative_numbers(self):
+        self.assertEqual(add(-1, -1), -2)
+
+    def test_add_mixed_numbers(self):
+        self.assertEqual(add(-1, 1), 0)
+
+# Dies führt den Test aus, wenn das Skript direkt ausgeführt wird
+if __name__ == '__main__':
+    unittest.main()
